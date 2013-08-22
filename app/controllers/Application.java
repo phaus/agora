d2c785291281f9ab3e223aee6880a48209ef8109
Philipp Haußleiter <philipp@haussleiter.de>
2013-08-22 11:32:31 +0200

    addine securesocial, adding Bootstrap 3

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,16 @@
 package controllers;
 
+import controllers.securesocial.SecureSocial;
 import models.User;
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.With;
 
-
+@With(SecureSocial.class)
 public class Application extends Controller {
+
     public static User activeUser;
+
     public static void index() {
         render();
     }
-
 }
